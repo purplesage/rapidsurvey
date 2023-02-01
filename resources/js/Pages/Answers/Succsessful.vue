f82c91b58a4d2c8ef377c46e26b0945aea1d6465
purpleSage <hectormza21@gmail.com>
2023-02-01 07:33:50 -0400

    successful page styling and logo change

--- resources/js/Pages/Answers/Succsessful.vue
+++ resources/js/Pages/Answers/Succsessful.vue
@@ -1,5 +1,7 @@
 <template>
-    <div class="h-screen w-full">
-        <h1 class="m-auto">Succsessful!</h1>
+    <div class="flex h-screen w-full items-center justify-center bg-green-200">
+        <h1 class="text-2xl text-green-500 md:text-3xl">
+            Answer sent, Thank you!
+        </h1>
     </div>
 </template>
