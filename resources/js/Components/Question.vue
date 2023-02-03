bc69865f05f8228e235f64ddbdc936c3b83a628f
purpleSage <hectormza21@gmail.com>
2023-02-02 20:24:07 -0400

    ...

--- resources/js/Components/Question.vue
+++ resources/js/Components/Question.vue
@@ -2,7 +2,7 @@
 import InputLabel from "@/Components/InputLabel.vue";
 import TextInput from "@/Components/TextInput.vue";
 import Option from "@/Components/Option.vue";
-import { ref, computed, onBeforeMount } from "vue";
+import { ref, onBeforeMount } from "vue";
 
 const props = defineProps({
     questionObject: Object,
@@ -41,22 +41,6 @@ onBeforeMount(() => {
 const deleteAllOptions = () => {
     props.questionObject.options = [];
 };
-
-const canUseAddButton = computed(() => {
-    const simpleInputs = ["text", "textarea"];
-
-    if (simpleInputs.includes(props.questionObject.type)) {
-        return props.questionObject.title?.length > 0;
-    } else {
-        return (
-            props.questionObject?.title?.length > 0 &&
-            props.questionObject?.options?.length > 0
-        );
-    }
-});
-
-//todo: inputsAreDisabled --rename--
-const disabled = ref(false);
 </script>
 <template>
     <div class="mt-10">
@@ -67,7 +51,6 @@ const disabled = ref(false);
                 v-model="questionObject.type"
                 name="question_type"
                 id="question_type"
-                :disabled="disabled"
                 @change="deleteAllOptions"
             >
                 <option value="text">Text</option>
@@ -80,21 +63,6 @@ const disabled = ref(false);
         <!-- text question -->
         <div class="mt-10">
             <div class="mt-5 flex space-x-3">
-                <button
-                    v-if="canUseAddButton"
-                    :disabled="disabled"
-                    class="inline-flex items-center rounded-md border border-transparent bg-green-600 px-4 py-2 text-xs font-semibold uppercase tracking-widest text-white transition duration-150 ease-in-out hover:bg-green-500 focus:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 active:bg-gray-900 disabled:bg-green-200 disabled:font-semibold disabled:text-slate-600"
-                    type="button"
-                    @click="disabled = true"
-                >
-                    {{ disabled ? "added" : "add" }}
-                </button>
-                <div
-                    class="inline-flex cursor-not-allowed items-center rounded-md border border-transparent bg-green-200 px-4 py-2 text-xs font-semibold uppercase tracking-widest text-slate-500"
-                    v-else
-                >
-                    add
-                </div>
                 <button
                     class="inline-flex items-center rounded-md border border-transparent bg-red-600 px-4 py-2 text-xs font-semibold uppercase tracking-widest text-white transition duration-150 ease-in-out hover:bg-gray-700 focus:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 active:bg-gray-900"
                     type="button"
@@ -117,7 +85,6 @@ const disabled = ref(false);
                         type="text"
                         class="mt-1 block w-full disabled:bg-green-100 disabled:text-green-700"
                         required
-                        :disabled="disabled"
                     />
                 </div>
                 <div class="mt-4">
@@ -128,7 +95,6 @@ const disabled = ref(false);
                         type="text"
                         class="mt-1 block w-full"
                         required
-                        :disabled="disabled"
                     />
                 </div>
             </div>
@@ -144,7 +110,6 @@ const disabled = ref(false);
                 <div class="mt-5">
                     <InputLabel for="questionTitle" value="Title" />
                     <TextInput
-                        :disabled="disabled"
                         v-model="questionObject.title"
                         id="questionTitle"
                         type="text"
@@ -155,7 +120,6 @@ const disabled = ref(false);
                 <div class="mt-4">
                     <InputLabel for="questionDescription" value="Description" />
                     <TextInput
-                        :disabled="disabled"
                         v-model="questionObject.description"
                         id="questionDescription"
                         type="text"
@@ -167,7 +131,6 @@ const disabled = ref(false);
                     <div class="flex justify-between">
                         <h3 class="text-xl font-bold">Options</h3>
                         <button
-                            :disabled="disabled"
                             type="button"
                             @click="addOption"
                             class="inline-flex items-center rounded-md border border-transparent bg-gray-800 px-4 py-2 text-xs font-semibold uppercase tracking-widest text-white transition duration-150 ease-in-out hover:bg-gray-700 focus:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 active:bg-gray-900 disabled:cursor-not-allowed disabled:bg-gray-500"
@@ -177,7 +140,6 @@ const disabled = ref(false);
                     </div>
                     <div class="mt-5 space-y-5">
                         <Option
-                            :disabled="disabled"
                             v-for="(option, index) in questionObject.options"
                             :key="option.id"
                             :optionObject="option"
